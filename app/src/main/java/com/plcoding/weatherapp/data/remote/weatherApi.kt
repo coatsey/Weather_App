e6f4ed27359f795e6db0cd12eef10b8dc7cc4ad3
Logan <coates.logan1125@gmail.com>
2022-11-09 13:43:05 -0700

    added Weather Repository

--- app/src/main/java/com/plcoding/weatherapp/data/remote/weatherApi.kt
+++ app/src/main/java/com/plcoding/weatherapp/data/remote/weatherApi.kt
@@ -4,7 +4,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 
-interface weatherApi {
+interface WeatherApi {
 
     @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
     suspend fun getWeatherData(
